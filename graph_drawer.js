42d0c86f34834c779bc6c57fc6171562a8b2526d
qwertyhoge <cood.gh47@hotmail.co.jp>
2018-03-19 01:08:24 +0900

    started to make class

--- graph_drawer.js
+++ graph_drawer.js
@@ -1,3 +1,39 @@
+class Node{
+    constructor(name, description){
+        name == undefined ? this.name = ""; : this.name = name;
+        description == undefined ? this.description = ""; : this.description = description;
+    }
+    
+    getName(){
+        return this.name;
+    }
+    getDescription(){
+        return this.description;
+    }
+    setName(name){
+        this.name = name;
+    }
+    setDescription(description){
+        this.description = description;
+    }
+    
+    // nodeArea内末尾にクラスがnodeのdivを追加する
+    generateTag(){
+        var newElement = document.createElement("div");
+        newElement.classList.add("node");
+        var parent = document.getElementById('nodeArea');
+        parent.insertBefore(newElement, null);
+        
+        newElement.innerHTML += name;
+    }
+    
+}
+
+function generateNewNode() {
+    var form = document.forms.nodeGenerator;
+    
+}
+
 var element;
 
 var handleMouseover;
@@ -5,7 +41,7 @@ var handleMouseout;
 var handleClick; 
 
 function setString(element, str) {
-    element.innerHTML = str;
+    element.innerHTML = "<p>" + str + "</p>";
 }
 
 function onLoad() {
