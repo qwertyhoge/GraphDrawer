5fd1a3c09e2838fba87ed7cad959a739bb1e0f09
qwertyhoge <cood.gh47@hotmail.co.jp>
2018-03-25 02:48:14 +0900

    it can be dragged

--- graph_drawer.js
+++ graph_drawer.js
@@ -1,7 +1,18 @@
+// todo: 要素のdivをドラッグして動かせるようにする クリックとドラッグのどちらかを判定する
+// グラフ要素のクラス
 class Node{
     constructor(name, description){
         name == undefined ? this.name = "" : this.name = name;
         description == undefined ? this.description = "" : this.description = description;
+        this.element = null;
+        
+        const self = this;
+        this.handleMouseupWrapper = function(event) {
+            self.handleMouseup(event);
+        };
+        this.handleMousemoveWrapper = function(event) {
+            self.handleMousemove(event);
+        };
     }
     
     getName(){
@@ -17,21 +28,63 @@ class Node{
         this.description = description;
     }
     
+    // マウスが離された(ドラッグ終了)ときのハンドラ
+    handleMouseup(event){
+        // 制御用のクラスを外したりイベントリスナーを解除する
+        this.element.classList.remove("dragging");
+        document.body.removeEventListener("mousemove", this.handleMousemoveWrapper, false);
+        this.element.removeEventListener("mouseup", this.handleMouseupWrapper, false);
+    }
+    
+    
+    // マウスがドラッグ中(押されていて動く)のときのハンドラ
+    handleMousemove(event){
+        // 元がbodyなのでthisでは取れない複数ドラッグしようとしたらどうなるんだろう
+        var dragElement = document.getElementsByClassName("dragging")[0];
+        
+        console.log(dragElement.style.left);
+        console.log(dragElement.style.top);
+        
+        // event.page*はマウスの座標なのでドラッグ中はこれに更新する
+        dragElement.style.position = "absolute";
+        dragElement.style.top = (event.pageY - 30) + "px";
+        dragElement.style.left = (event.pageX - 30) + "px";
+    }
+    
+    // ここからのthisはNodeではなくイベント元のdiv
+    // なのだが、addTag()内でNodeを指すように細工した
+    // マウスが押されたときのハンドラ
+    handleMousedown(event){
+        // bodyでドラッグを検知する分、識別用の何かが必要でクラスだと拡張性が高い(多分)
+        this.element.classList.add("dragging");
+        
+        // ここでも同様の細工をするのだが、即時関数だとremoveEventListenerができなくなるのでここでwrapperとして名前を付けて定義しておく
+        document.body.addEventListener("mousemove", this.handleMousemoveWrapper, false);
+        this.element.addEventListener("mouseup", this.handleMouseupWrapper, false);
+    }
+
     // nodeArea内末尾にクラスがnodeのdivを追加する
     addTag(){
-        var newElement = document.createElement("div");
-        newElement.classList.add("node");
-  //      newElement.addEventListener("mousedown", handleMousedown, false);
+        var self = this;
+        this.element = document.createElement("div");
+        this.element.classList.add("node");
+        this.element.addEventListener("mousedown", function(event){
+            // 即時関数にthisがdivになるのを吸収させて、内部でself(Node)からhandleMousedownを呼び出すことでhandleMousedown内でのカレントオブジェクトをNodeにする
+            return self.handleMousedown(event);
+        }, false);
         
         var parent = document.getElementById("nodeArea");
         // null指定で末尾になる
-        parent.insertBefore(newElement, null);
+        parent.insertBefore(this.element, null);
         
-        newElement.innerHTML += this.name;
+        this.element.innerHTML += this.name;
+        
     }
     
 }
 
+// todo: IDを同時に付けるなどし、削除できるようにする
+// クラスを生成し、divを追加する
 function addNewNode() {
     var form = document.forms.nodeGenerator;
     var name, description;
@@ -43,37 +96,4 @@ function addNewNode() {
     node = new Node(name, description);
 
     node.addTag();
-}
-
-var element;
-
-var handleMouseover;
-var handleMouseout;
-var handleClick; 
-
-function setString(element, str) {
-    element.innerHTML = "<p>" + str + "</p>";
-}
-
-function onLoad() {
-    element = document.getElementById("testNode");
-    handleMouseover = function(){
-        setString(element, "mouse over.");
-    }
-    handleMouseout = function(){
-        setString(element, "mouse out.");
-    }
-    handleClick = function(event){
-        setString(element, "clicked.");
-        element.removeEventListener("mouseover", handleMouseover, false);
-        element.removeEventListener("mouseout", handleMouseout, false);
-    }
-
-    
-    console.log("element:" + element);
-    
-    element.addEventListener("mouseover", handleMouseover, false);
-    element.addEventListener("mouseout", handleMouseout, false);
-    element.addEventListener("click", handleClick, false);
-
 }
