fe458ea2f41995329c3e316e49fc3938e545c1b7
qwertyhoge <cood.gh47@hotmail.co.jp>
2018-04-04 20:01:24 +0900

    arrows(single for one node) move with dragging node

--- graph_drawer.js
+++ graph_drawer.js
@@ -5,7 +5,9 @@ class Node{
         name == undefined ? this.name = "" : this.name = name;
         description == undefined ? this.description = "" : this.description = description;
         this.element = null;
-        this.connection = null;
+        this.connectionToList = null;
+        this.arrowElem = null;
+        this.arrowNum = null;
         
         const self = this;
         this.handleMouseupWrapper = function(event) {
@@ -52,6 +54,8 @@ class Node{
         // event.page*はマウスの座標なのでドラッグ中はこれに更新する
         this.element.style.top = (event.pageY - 30) + "px";
         this.element.style.left = (event.pageX - 30) + "px";
+        
+        updateArrows(this);
     }
     
     // ここからのthisはNodeではなくイベント元のdiv
@@ -68,12 +72,14 @@ class Node{
     }
 
     // nodeArea内末尾にクラスがnodeのdivを追加する
-    addTag(count){
+    addTag(nodeCount){
         var self = this;
         this.element = document.createElement("div");
         this.element.classList.add("node");
         // divにNodeのリストに対応するidを付けて、divから辿れるようにする
-        this.element.id = "node" + count;
+        this.element.id = "node" + nodeCount;
+        this.element.style.top = "50px";
+        this.element.style.left = "50px";
         
         // 即時関数にthisがdivになるのを吸収させて、内部でself(=Node)からhandleMousedownを呼び出すことでhandleMousedown内でのカレントオブジェクトをNodeにする
         this.element.addEventListener("mousedown", function(event){
@@ -94,6 +100,7 @@ function onLoad(){
     infoForm.edit.addEventListener("click", editNodeInformation, false);
     infoForm.delete.addEventListener("click", deleteNode, false);
     connectorForm.connect.addEventListener("click", prepareConnectNode, false);
+    connectorForm.cancel.addEventListener("click", clearConnecting, false);
 }
 
 // 複数ボタンを無効化する場面が多いのでまとめた
@@ -107,7 +114,9 @@ function disableButtons(){
 }
 
 var nodeList = [];
-var count = 0;
+var arrowList = [];
+var nodeCount = 0;
+var arrowCount = 0;
 
 // クラスを生成し、divを追加する
 function addNewNode() {
@@ -122,8 +131,8 @@ function addNewNode() {
     
     nodeList.push(node);
 
-    node.addTag(count);
-    count++;
+    node.addTag(nodeCount);
+    nodeCount++;
     
     form.name.value = "";
     form.description.value = "";
@@ -151,7 +160,8 @@ function editNodeInformation(event) {
 }
 
 // 文字列内で最初に見つかった数値を返す
-function myParseIntEnd(id){
+// ここでは配列の添え字がIDの最後に仕込んであるのでそれを取得するために使う
+function parseNum(id){
     return parseInt((/\d+/.exec(id))[0], 10);
 }
 
@@ -164,7 +174,7 @@ function applyNodeInformation(event){
     var form = document.forms.nodeInformation;
     var connectButton = document.forms.nodeConnector.connect;
     var selectedElement = document.getElementsByClassName("selected")[0];
-    var selectedNode = nodeList[myParseIntEnd(selectedElement.id)];
+    var selectedNode = nodeList[parseNum(selectedElement.id)];
     
     console.log("index:" + selectedElement.id);
     console.log(selectedNode);
@@ -173,7 +183,7 @@ function applyNodeInformation(event){
     selectedNode.description = form.description.value;
     
     form.delete.disabled = false;
-    connectButton.disabled = false;
+    connectButton.disabled = true;
     
     updateInformation(selectedNode.name, selectedNode.description);
 }
@@ -188,14 +198,10 @@ function updateInformation(name, description){
     infoForm.name.value = name;
     infoForm.description.value = description;
     
-    // 編集中・結合中の状態をリセットする
+    // 編集中の状態をリセットする
     infoForm.edit.value = "edit";
     infoForm.edit.removeEventListener("click", applyNodeInformation, false);
     infoForm.edit.addEventListener("click", editNodeInformation, false);
-    connectorForm.connect.value = "connect";
-    connectorForm.connect.removeEventListener("click", connectNode, false);
-    connectorForm.connect.addEventListener("click", prepareConnectNode, false);
-    
 }
 
 // 選択状態をクリアするハンドラ。 nodeAreaのonmousedownに設定してある。
@@ -204,13 +210,14 @@ function updateInformation(name, description){
 function selectClear(event){
     console.log("clearing");
     var selectedElement = document.getElementsByClassName("selected")[0];
-    var editButton = document.forms.nodeInformation.edit;
-    var deleteButton = document.forms.nodeInformation.delete;
+    var connectingToElement = document.getElementsByClassName("connectingTo")[0];
+    var connectButton = document.forms.nodeConnector.connect;
     if(selectedElement != undefined){
         selectedElement.classList.remove("selected");
-        if(selectedElement.classList.contains("connecting")){
-            selectedElement.classList.remove("connecting");
-        }
+    }
+    if(connectingToElement != undefined){
+        connectingToElement.classList.remove("connectingTo");
+        connectButton.disabled = true;
     }
     disableButtons();
     updateInformation("-", "-");
@@ -219,16 +226,30 @@ function selectClear(event){
 
 function deleteNode(event){
     var selectedElement = document.getElementsByClassName("selected")[0];
-    var selectedIndex = myParseIntEnd(selectedElement.id);
+    var selectedIndex = parseNum(selectedElement.id);
     var parentElement = selectedElement.parentElement;
     var editButton = document.forms.nodeInformation.edit;
     var deleteButton = document.forms.nodeInformation.delete;
     
+    // コネクションを持ったノードがこれを宛先にしていれば削除する
+    nodeList.forEach(function(node){
+        if(node.connectionTo === node){
+            for(var i = 0; i < arrowList.length; i += 1){
+                if(node.connectionTo === arrowList[i]){
+                    arrowList.splice(i, 1);
+                }
+                arrowList[i].id = "arrow" + i;
+            }
+            node.connectionTo = null;
+            arrowCount -= 1;
+        }
+    });
+    
     parentElement.removeChild(selectedElement);
     nodeList.splice(selectedIndex, 1);
-    count -= 1;
+    nodeCount -= 1;
     
-    for(var i = selectedIndex; i < count; i += 1){
+    for(var i = selectedIndex; i < nodeCount; i += 1){
         nodeList[i].element.id = "node" + i;
     }
     
@@ -240,6 +261,7 @@ function prepareConnectNode(event){
     var infoForm = document.forms.nodeInformation;
     var connectorForm = document.forms.nodeConnector;
     var selectedElement = document.getElementsByClassName("selected")[0];
+    var nodeAreaElement = document.getElementById("nodeArea");
     
     // selectedクラスを持つdivがある場合に処理をする。
     if(selectedElement != undefined){
@@ -248,19 +270,143 @@ function prepareConnectNode(event){
         infoForm.edit.disabled = true;
         infoForm.delete.disabled = true;
         connectorForm.connect.value = "apply";
+        connectorForm.connect.disabled = true;
+        connectorForm.cancel.disabled = false;
         selectedElement.classList.add("connecting");
         selectedElement.classList.remove("selected");
-        infoForm.edit.removeEventListener("click", prepareConnectNode, false);
-        infoForm.edit.addEventListener("click", connectNode, false);
+        connectorForm.connect.removeEventListener("click", prepareConnectNode, false);
+        connectorForm.connect.addEventListener("click", connectNode, false);
+        nodeAreaElement.addEventListener("click", selectConnectNode, false);
         console.log("start connect");
     }
 }
 
-function connectNode(event){
-    var selectedElement = document.getElementsByClassName("selected")[0];
+// nodeを宛先にするノードがあればそこの矢印を更新する
+function updateArrows(node){
+    var i;
+    var targetNode;
+    var arrow;
     
-    // 自分自身でない要素がselectedクラスを持っていた場合に処理をする
-    if(selectedElement != undefined && !(selectedElement.classList.contains("connecting"))){
-        infoForm.name.disabled = 
+    // 複数にはまだ対応していない
+    // 矢尻をarrowの子にしているのでこれで取得できる
+    if(node.connectionTo != null){
+        setArrow(node.element, node.connectionTo, node.arrowElem, node.arrowElem.children[0]);
     }
+    
+    // 自分を宛先にしている矢印も更新しないといけない
+    // nodeListを全探索して自分を宛先にしているやつを探す
+    nodeList.forEach(function(listNode, index){
+        if(listNode.connectionTo === node.element){
+            setArrow(listNode.element, node.element, listNode.arrowElem, listNode.arrowElem.children[0]);
+        }
+    });
+}
+
+function setArrow(connect, connectTo, arrow, arrowTip){
+    var distance;
+    var angle;
+    const halfNodeSize = 75;
+    const arrowSize = 20;
+    
+    if(connect == null){
+        return;
+    }
+    
+    distance = Math.sqrt(Math.pow(parseInt(connectTo.style.left) - parseInt(connect.style.left), 2) +  Math.pow(parseInt(connectTo.style.top) - parseInt(connect.style.top), 2));
+    
+    // topは下が正なので引き算が逆になる
+    angle = Math.atan2(parseInt(connect.style.top) - parseInt(connectTo.style.top), parseInt(connectTo.style.left) - parseInt(connect.style.left));
+    
+    // 使うべき三角関数がsinの場合とcosの場合で場合分け
+    if(Math.abs(angle) >= Math.PI/4　&& Math.abs(angle) <= Math.PI*3/4){
+        // どの向きだろうと符号の概念は出てきてほしくないのでabsを取る
+        arrow.style.width = (distance - Math.abs(halfNodeSize/Math.sin(angle)) - arrowSize) + "px";
+        console.log("calced by sin" + "sin(angle):" + Math.sin(angle));
+    }else{
+        arrow.style.width = (distance - Math.abs(halfNodeSize/Math.cos(angle)) - arrowSize) + "px";
+        console.log("calced by cos" + "cos(angle):" + Math.cos(angle));
+    }
+    
+    arrow.style.top = (parseInt(connect.style.top) + halfNodeSize) + "px";
+    arrow.style.left = (parseInt(connect.style.left) + halfNodeSize) + "px";
+    arrowTip.style.left = arrow.style.width;
+    
+    /* 右が0で時計回りに 0<=θ<2π で増えるように変換する */
+    angle *= -1;
+    if(angle < 0){
+        angle += 2*Math.PI;
+    }
+    arrow.style.transform = "rotateZ(" + angle*180/Math.PI + "deg)";
+    
+}
+
+function putArrow(connect, connectTo){
+    var newArrowElem = document.createElement("div");
+    var newArrowTipElem = document.createElement("div");
+    var parent = document.getElementById("nodeArea");
+    var connectNode = nodeList[parseNum(connect.id)];
+    
+    setArrow(connect, connectTo, newArrowElem, newArrowTipElem);
+    
+    newArrowElem.classList.add("connectArrow");
+    newArrowTipElem.classList.add("connectArrowTip");
+    newArrowElem.id = "arrow" + arrowCount;
+    
+    parent.insertBefore(newArrowElem, connect.nextElementSibling);
+    newArrowElem.insertBefore(newArrowTipElem, null);
+    
+    connectNode.arrowElem = newArrowElem;
+    connectNode.arrowNum = arrowCount;
+    
+    arrowList.push(newArrowElem);
+    arrowCount += 1;
+}
+
+function connectNode(event){
+    var connectingElement = document.getElementsByClassName("connecting")[0];
+    var connectingToElement = document.getElementsByClassName("connectingTo")[0];
+    var connectingNode = nodeList[parseNum(connectingElement.id)];
+
+    connectingNode.connectionTo = connectingToElement;
+    putArrow(connectingElement, connectingToElement);
+
+    clearConnecting(event);
+}
+
+function selectConnectNode(event){
+    console.log("selecting connectNode");
+    var infoForm = document.forms.nodeInformation;
+    var selectedElement = document.getElementsByClassName("selected")[0];
+    var connectButton = document.forms.nodeConnector.connect;
+    
+    // 自分自身でない要素がselectedクラスを持っていた場合にconnectingToを付加する
+    if(selectedElement != undefined){
+        if(!selectedElement.classList.contains("connecting")){
+            selectedElement.classList.add("connectingTo");
+            selectedElement.classList.remove("selected");
+        }else{
+            connectButton.disabled = true;
+        }
+    }
+
+    infoForm.edit.disabled = true;
+    infoForm.delete.disabled = true;
+}
+
+function clearConnecting(event){
+    var connectingToElement = document.getElementsByClassName("connectingTo")[0];
+    var connectingElement = document.getElementsByClassName("connecting")[0];
+    var connectorForm = document.forms.nodeConnector;
+    var nodeAreaElement = document.getElementById("nodeArea");
+    
+    connectorForm.connect.addEventListener("click", prepareConnectNode, false);
+    connectorForm.connect.removeEventListener("click", connectNode, false);
+    connectorForm.connect.disabled = true;
+    connectorForm.connect.value = "connect";
+    connectorForm.cancel.disabled = true;
+    nodeAreaElement.removeEventListener("click", selectConnectNode, false);
+    if(connectingToElement != undefined){
+        connectingToElement.classList.remove("connectingTo");
+    }
+    connectingElement.classList.remove("connecting");
 }
